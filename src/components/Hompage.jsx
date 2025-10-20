3af6c2715b902070420d151f9c093ee6ede38130
Dan131195 <daniele.franceschini1395@gmail.com>
2025-10-21 01:19:58 +0200

    .

--- src/components/Hompage.jsx
+++ src/components/Hompage.jsx
@@ -77,9 +77,9 @@ function Homepage({ city }) {
       {weatherData ? (
         <div>
           <div className="currentWeatherContainer container border border-1 rounded-4 p-3 ">
-            <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex justify-content-between align-items-center ">
               <div>
-                <p className="fs-4">
+                <p className="fs-3">
                   <img src={locationIcon} />
                   {weatherData.name}{" "}
                   <span className=" fs-6 opacity-75">
@@ -99,25 +99,66 @@ function Homepage({ city }) {
               />
             </div>
 
-            <p className="fs-5 ps-2">
-              {/*  Per mettere la maiuscola della prima lettera ⬇️⬇️ */}
-              {weatherData.weather[0].description.charAt(0).toUpperCase() +
-                weatherData.weather[0].description.slice(1)}{" "}
+            <p className="fs-5 ps-2 capitalize-first">
+              {weatherData.weather[0].description}
               <span className="ps-2">
                 {Math.floor(weatherData.main.temp_min)}° /{" "}
                 {Math.floor(weatherData.main.temp_max)}°
               </span>
             </p>
 
-            <div className="ps-2 row row-cols-2 row-cols-lg-3">
-              <div className="border border-1 border-light">
-                <img src="" alt="" />
-                <p></p>
-                <p></p>
+            <div className=" row row-cols-2 row-cols-lg-3">
+              <div className=" ps-4 pe-2">
+                <div className="border border-1 border-light text-center rounded-4 py-2 detailsContainer">
+                  <img src={windIcon} alt="Wind Icon" className="hompageIcon" />
+                  <p className="m-0 fs-5 fw-bold">
+                    {Math.floor(weatherData.wind.speed)} Km/h
+                  </p>
+                  <p className="detailsTitles m-0 opacity-75">VENTO</p>
+                </div>
               </div>
-              <div></div>
-              <div></div>
-              <div></div>
+
+              <div className=" pe-4 ps-2">
+                <div className="border border-1 border-light text-center rounded-4 py-2 detailsContainer">
+                  <img
+                    src={humidityIcon}
+                    alt="Humidity Icon"
+                    className="hompageIcon"
+                  />
+                  <p className="m-0 fs-5 fw-bold m-0">
+                    {weatherData.main.humidity}%
+                  </p>
+                  <p className="detailsTitles m-0 opacity-75">UMIDITÀ</p>
+                </div>
+              </div>
+
+              <div className="py-3 ps-4 pe-2">
+                <div className="border border-1 border-light text-center rounded-4 py-2 detailsContainer">
+                  <img
+                    src={visibilityIcon}
+                    alt="Humidity Icon"
+                    className="hompageIcon"
+                  />
+                  <p className="m-0 fs-5 fw-bold m-0">
+                    {weatherData.visibility} m
+                  </p>
+                  <p className="detailsTitles m-0 opacity-75">VISIBILITÀ</p>
+                </div>
+              </div>
+
+              <div className="py-3 pe-4 ps-2">
+                <div className="border border-1 border-light text-center rounded-4 py-2 detailsContainer">
+                  <img
+                    src={thermometerIcon}
+                    alt="Humidity Icon"
+                    className="hompageIcon"
+                  />
+                  <p className="m-0 fs-5 fw-bold m-0">
+                    {weatherData.main.pressure} hpA
+                  </p>
+                  <p className="detailsTitles m-0 opacity-75">PERCEPITA</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
